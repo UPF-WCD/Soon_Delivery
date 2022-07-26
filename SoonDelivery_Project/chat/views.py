930a0c926e4455966105eefca187b9b94b9d0fa8
Chae hee chan <eqholic1125@gmail.com>
2022-07-26 16:36:05 +0900

    feat: 채팅방 생성시 제목 표시

--- SoonDelivery_Project/chat/views.py
+++ SoonDelivery_Project/chat/views.py
@@ -14,31 +14,27 @@ def chat(request, user_id = 0):
       'rooms': rooms
     })
 
-def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name
-    })
+def room(request, room_id):
+    room = get_object_or_404(Chat, id="room_id")
+    return render(request, 'chat/room.html', {'room': room})
 
 def create_room(request):
   if request.method == 'POST':
     chatings = Chat.objects.filter(Q(user1 = User.objects.get(id = request.POST["user1_id"])) & Q(user2 = User.objects.get(id = request.POST["user2_id"])))
     if len(chatings) == 0:
-      print("1")
       chatings = Chat.objects.filter(Q(user1 = User.objects.get(id = request.POST["user2_id"])) & Q(user2 = User.objects.get(id = request.POST["user1_id"])))
       if len(chatings) == 0:
-        print("2에서 걸림")
         new_room = Chat()
+        new_room.room_name = str(request.POST["user1_id"]) + ", " + request.POST["user2_id"] + "의 채팅방" # 방 제목
         new_room.user1 = User.objects.get(id = request.POST["user1_id"]) # 배달자(본인)
         new_room.user2 = User.objects.get(id = request.POST["user2_id"]) # 주문자
         new_room.save()
-
         return redirect('start_delivery', room_id=new_room.id, user_id=request.POST["user1_id"], order_id=request.POST["order_id"])
+
       else: #채팅방이 있는 경우 2
-        print("3에서 걸림")
         return redirect('start_delivery', room_id=chatings[0].id,  user_id=request.POST["user1_id"], order_id=request.POST["order_id"])
 
     else: # 채팅방이 있는 경우 1
-      print("4에서 걸림")
       return redirect('start_delivery',  room_id=chatings[0].id, user_id=request.POST["user1_id"], order_id=request.POST["order_id"])
 
   else:
